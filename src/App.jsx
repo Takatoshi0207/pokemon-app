98d9e182007209d97e8fc70a45ffb5157f31bfc4
Takatoshi0207 <t.kotani@m-okuchu.com>
2022-10-28 18:19:32 +0900

    Title barの設定

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import Card from "./components/Card/Card";
+import Navbar from "./Navbar/Navbar";
 import { getAllPokemon, getPokemon } from "./utils/pokemon";
 
 function App() {
@@ -33,20 +34,23 @@ function App() {
   console.log(pokemonData)
 
   return (
-    <div className="App">
-      {loading ? (
-        <h1>ロード中・・・</h1>
-      ) : (
-        <>
-          <div className="pokemonCardContainer">
-            {/* i は index */}
-            {pokemonData.map((pokemon, i) => {
-              return <Card key={i} pokemon={pokemon} />;
-            })}
-          </div>
-        </>
-      )}
-    </div>
+    <>
+      <Navbar />
+      <div className="App">
+        {loading ? (
+          <h1>ロード中・・・</h1>
+        ) : (
+          <>
+            <div className="pokemonCardContainer">
+              {/* i は index */}
+              {pokemonData.map((pokemon, i) => {
+                return <Card key={i} pokemon={pokemon} />;
+              })}
+            </div>
+          </>
+        )}
+      </div>
+    </>
   );
 }
 
